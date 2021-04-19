d99230a7ec3b9948b6385bcf833385d0ccea98ea
ta.nguyen <ta.nguyen@TANGUYEN>
2021-04-19 09:01:48 +0700

    add Constants

---
+++ SendQueueAPI/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SendQueueAPI
+{
+    public static class Constant
+    {
+        public const string CreatePatient = "api/patient/createpatient";
+        public const string UpdatePatient = "api/patient/updatepatient";
+    }
+}
